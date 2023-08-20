004d16653eb596da85a78b12c813ecf8050473ee
Josef <josefheers@googlemail.com>
2023-08-20 13:56:52 +0200

    writer

--- fb.py
+++ fb.py
@@ -1,17 +1,31 @@
+from functools import reduce
+
 class Driver():
  def __init__(self, name = ''): self.name = name
  def __str__(self): return str(self.name)
  def __eq__(self, other): return self.name == other.name
  def valid(self): return len(self.name) > 0
+ def write(self): return str(self.name)
  
 class Trip():
  def __init__(self, start, end, driver ): self.start = start; self.end=end; self.driver = driver;
  def __str__(self): return f'Driver {self.driver}: from {self.start} to {self.end}'
  def valid(self): return self.end > self.end and self.driver.valid()
  def dist(self): return max(0,self.end - self.end)
+ def write(self): return f'{self.start}\t{self.end}\t{self.driver}' 
  
 class Bill():
  def __init__(self, amount, driver ): self.amount = amount; self.driver = driver;
  def __str__(self): return f'Driver {self.driver}: amount = {self.amount}€'
  def valid(self): return self.amount > 0 and self.driver.valid()
+ def write(self): return f'{self.amount}\t{self.driver}'
  
+class Billing():
+ def __init__(self, name=''): self.name = name; self.bills = []; self.trips = []; self.insurance = 0.05;
+ def __str__(self): return f'Billing "{self.name}"'
+ def appendTrip(self, trip): self.trips.append(trip)  
+ def appendBill(self, bill): self.bills.append(bill)  
+ def valid(self): return true;
+ def writeTrips(self): return reduce(lambda s,x: str(s) + x.write()+'\n', self.trips,'').strip() 
+ def writeBills(self): return reduce(lambda s,x: str(s) + x.write()+'\n', self.bills,'').strip() 
+
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from fb import Driver, Trip, Bill
+from fb import Driver, Trip, Bill, Billing
 
 print(Driver('Josef'));
 print('cmp drivers: ' + str(Driver('Josef')==Driver('Josef2')))
@@ -7,5 +7,9 @@ print('cmp drivers: ' + str(Driver('Heers')==Driver('Heers')))
 print('validate default: ' + str(Driver().valid()))
 print('validate empty: ' + str(Driver('').valid()))
 print('validate not empty: ' + str(Driver('a').valid()))
-print(Trip(10,100,Driver('Jannis')))
-print(Bill(33.44,Driver('Jannis')))
+print(Trip(10,100,Driver('Jannis')).write())
+print(Bill(33.44,Driver('Jannis')).write())
+b = Billing('Test') 
+b.appendTrip(Trip(0,100,Driver('Josef')))
+b.appendTrip(Trip(100,110,Driver('Janns')))
+print(b.writeTrips())
