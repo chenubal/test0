35b888e8e3808f6af5c1eaa641882ef20ec8850c
Josef <josefheers@googlemail.com>
2023-08-21 09:44:38 +0200

    layouts

--- app-win.py
+++ app-win.py
@@ -2,13 +2,12 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from fb_view import createFBWidget
 
-class MainWindow(QMainWindow):
- def __init__(self):
-  super().__init__()
-  self.setWindowTitle("Fartenbuch")
-  self.setCentralWidget(createFBWidget(self))
-
 app = QApplication(sys.argv)
-window = MainWindow()
+window = QMainWindow()
+view = createFBWidget(window);
+
+window.setWindowTitle("Fartenbuch")
+window.setCentralWidget(view)
 window.show()
+
 app.exec()
