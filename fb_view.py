a94f75a2d0125a8afab3a0f0b07e8b4df0301065
Josef <josefheers@googlemail.com>
2023-08-23 09:22:46 +0200

    ref

--- fb_view.py
+++ fb_view.py
@@ -1,18 +1,28 @@
 import PySide6 .QtWidgets as W
 
-def layoutBillingList(model):
-  listwidget = W.QListWidget()
-  listwidget.setMaximumSize(300, 800)
-  for x in model.pathes: 
-    listwidget.addItem(x.name)
-  listwidget.currentRowChanged.connect(model.select)
-  if len(model.pathes) > 0: listwidget.setCurrentRow(0)
+class Pathes_Widget(W.QWidget):
+  def __init__(self, model):
+    super().__init__()
+    self.m = model
+    self.lw = self.makeWidgets(self.m)
+    self.fillList()
  
-  vbox = W.QVBoxLayout()
-  vbox.addWidget(W.QLabel('Anrechnungen:'))
-  vbox.addWidget(listwidget)
-  return vbox
-
+    vbox = W.QVBoxLayout()
+    vbox.addWidget(W.QLabel('Abrechnungen:'))
+    vbox.addWidget(self.lw) 
+    self.setLayout(vbox)
+    
+  def makeWidgets(self,model):
+    lw = W.QListWidget()
+    lw.setMaximumSize(300, 800)
+    lw.currentRowChanged.connect(model.select)
+    return lw
+ 
+  def fillList(self):
+     self.lw.clear()
+     for p in self.m.pathes: self.lw.addItem(p.name)
+     if len(self.m.pathes) > 0: self.lw.setCurrentRow(0)
+ 
 def layoutBilling():
   button = W.QPushButton("Mid!")
   button.setCheckable(True)
@@ -35,7 +45,7 @@ class BillingDB_Widget(W.QWidget):
   def __init__(self, model):
     super().__init__()
     hbox = W.QHBoxLayout()
-    hbox.addLayout(layoutBillingList(model))
+    hbox.addWidget(Pathes_Widget(model))
     hbox.addLayout(layoutBilling())
     hbox.addLayout(layoutResult())
     self.setLayout(hbox)
