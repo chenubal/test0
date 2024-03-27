64ec0705259c5af5d087849b0adb190e629c5ce4
Josef <josefheers@googlemail.com>
2024-03-27 13:49:10 +0100

    Kleine Änderungen

--- fb.py
+++ fb.py
@@ -27,6 +27,7 @@ class Trip():
   ''' Trip class manages trip data [start,end] of a driver'''
   def __init__(self, start=0, end=0, driver='' ): self.start = int(start); self.end=int(end); self.driver = Driver(driver);
   def __str__(self): return f'Driver {self.driver}: from {self.start} to {self.end}'
+  def __eq__(self, other): return (self.start, self.end, self.driver) == (other.start, other.end, other.driver)
 
   def valid(self): return self.end > self.end and self.driver.valid()
   def dist(self): return max(0,self.end - self.start)
@@ -45,6 +46,7 @@ class Bill():
   ''' Bill class manages bill data [amount] of a driver'''
   def __init__(self, amount=0, driver='' ): self.amount = float(amount); self.driver = Driver(driver);
   def __str__(self): return f'Driver {self.driver}: amount = {self.amount}€'
+  def __eq__(self, other): return (self.amount, self.driver) == (other.amount, other.driver)
 
   def valid(self): return self.amount > 0 and self.driver.valid()
   def write(self): return '\t'.join([str(self.amount), str(self.driver)])
@@ -90,25 +92,27 @@ class Billing():
  
   def report(self):
     ''' compiles a report to a string'''
+    maintainRate = 0.07
     totalTrips = sum(x.dist() for x in self.trips) 
     totalBills = sum(x.amount for x in self.bills) 
-    totalEnsure = totalTrips * 0.05
-    total = totalEnsure+totalBills
+    totalMaintain = totalTrips * maintainRate
+    total = totalMaintain + totalBills
     s = f'----------------- {Path(self.name).name} -----------------------\n'
     s += f'Reisen gesamt:  {totalTrips}km\n'
     s += f'Tanken gesamt:  {totalBills}€\n'
-    s += f'Versich. gesamt:  {round(totalEnsure,2)}€\n'
-    s += f'Kosten gesamt:  {total}€\n'
+    s += f'Nebenkosten  :  {round(totalMaintain,2)}€\n'
+    s += f'Kosten gesamt:  {round(total,2)}€\n'
     if totalTrips ==0: return s
     s += f'Quote (R):  {round(100*totalBills/totalTrips,2)} ct/km\n'
     s += f'Quote (T):  {round(100*total/totalTrips,2)} ct/km\n'
-    s += f'Versicherung:  5.00 ct/km\n'
+    s += f'Nebenkosten:  {round(maintainRate,2)} €/km\n'
     for drv in self.allDriver():
       dBill = sum(x.amount for x in self.bills if x.driver==drv) 
       dRatio = sum(x.dist() for x in self.trips if x.driver==drv)/totalTrips
       dTotal = dRatio*total
       s += f'-------------------------------\n'
       s += f'Fahrer: {str(drv)}\n'
+      s += f'Strecke = {round(dRatio*totalTrips,2)} km\n'
       s += f'Quote = {round(dRatio,2)}\n'
       s += f'Anteil = {round(dTotal,2)}€\n'
       s += f'Bezahlt = {round(dBill,2)}€\n'
