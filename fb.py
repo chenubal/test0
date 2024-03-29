dd304985dfb15363fd126a1fbfc59e6ad50d43a9
Josef <josefheers@googlemail.com>
2024-03-29 11:20:14 +0100

    Refactor

--- fb.py
+++ fb.py
@@ -16,23 +16,39 @@ def getSubDirs(path = getDBPath() ):
 
 class Driver():
   ''' Driver class modles a driver'''
-  def __init__(self, name = ''): self.name = name
-  def __str__(self): return str(self.name)
-  def __eq__(self, other): return str(self.name) == str(other.name)
+  def __init__(self, name = ''): 
+    self.name = name
+  def __str__(self): 
+    return str(self.name)
+  def __eq__(self, other): 
+    return str(self.name) == str(other.name)
+  def __hash__(self):
+    return hash(str(self))
 
-  def valid(self): return len(self.name) > 0
-  def write(self): return str(self.name)
+  def valid(self): 
+    return len(self.name) > 0
+  def write(self): 
+    return str(self.name)
  
 class Trip():
   ''' Trip class manages trip data [start,end] of a driver'''
-  def __init__(self, start=0, end=0, driver='' ): self.start = int(start); self.end=int(end); self.driver = Driver(driver);
-  def __str__(self): return f'Driver {self.driver}: from {self.start} to {self.end}'
-  def __eq__(self, other): return (self.start, self.end, self.driver) == (other.start, other.end, other.driver)
-
-  def valid(self): return self.end > self.end and self.driver.valid()
-  def dist(self): return max(0,self.end - self.start)
-  def write(self): return '\t'.join([str(self.start), str(self.end), str(self.driver)])
-  def read(self, serial) : self = readTrip(serial); return self
+  def __init__(self, start=0, end=0, driver='' ): 
+    self.start = int(start) 
+    self.end=int(end)
+    self.driver = Driver(driver)
+  def __str__(self): 
+    return f'Driver {self.driver}: from {self.start} to {self.end}'
+  def __eq__(self, other): 
+    return (self.start, self.end, self.driver) == (other.start, other.end, other.driver)
+  def valid(self): 
+    return self.end > self.end and self.driver.valid()
+  def dist(self): 
+    return max(0,self.end - self.start)
+  def write(self): 
+    return '\t'.join([str(self.start), str(self.end), str(self.driver)])
+  def read(self, serial) : 
+    self = readTrip(serial); 
+    return self
  
 def readTrip(serial) :
   ''' returns a Trip object from a serial string'''
@@ -61,14 +77,20 @@ def readBill(serial) :
   
 class Billing():
   ''' Billing class manages bills and trips and gives an report'''
-  def __init__(self, name=''): self.name = name; self.bills = []; self.trips = []; self.insurance = 0.05;
+  def __init__(self, name=''): 
+    self.name = name 
+    self.bills = [] 
+    self.trips = []
+    self.opRate = 0.05 # operating cost €/km
   def __str__(self): return f'Billing "{self.name}"'
 
   def appendTrip(self, trip): self.trips.append(trip)  
   def appendBill(self, bill): self.bills.append(bill)  
   def clear(self) : self.trips=[]; self.bills= []
-  def writeTrips(self): return '\n'.join(map(lambda t: t.write(), self.trips)) 
-  def writeBills(self): return '\n'.join(map(lambda b: b.write(), self.bills)) 
+  def writeTrips(self): 
+    return '\n'.join(map(lambda t: t.write(), self.trips)) 
+  def writeBills(self): 
+    return '\n'.join(map(lambda b: b.write(), self.bills)) 
 
   def load(self, dir):
     ''' load billing data from a folder'''
@@ -84,11 +106,8 @@ class Billing():
     storeFile(dir + '/trips.txt',self.writeTrips()) 
 
   def allDriver(self):
-    ''' collects a driver from bills and trips'''
-    r = []
-    [r.append(x.driver) for x in self.trips if x.driver not in r ]
-    [r.append(x.driver) for x in self.bills if x.driver not in r ]
-    return r
+    ''' collects all driver from bills and trips'''
+    return {x.driver for x in self.trips} | {x.driver for x in self.bills}
  
   def report(self):
     ''' compiles a report to a string'''
@@ -96,25 +115,25 @@ class Billing():
     totalTrips = sum(x.dist() for x in self.trips) 
     totalBills = sum(x.amount for x in self.bills) 
     totalMaintain = totalTrips * maintainRate
-    total = totalMaintain + totalBills
+    totalAmount = totalMaintain + totalBills
     s = f'----------------- {Path(self.name).name} -----------------------\n'
     s += f'Reisen gesamt:  {totalTrips}km\n'
     s += f'Tanken gesamt:  {totalBills}€\n'
     s += f'Nebenkosten  :  {round(totalMaintain,2)}€\n'
-    s += f'Kosten gesamt:  {round(total,2)}€\n'
+    s += f'Kosten gesamt:  {round(totalAmount,2)}€\n'
     if totalTrips ==0: return s
     s += f'Quote (R):  {round(100*totalBills/totalTrips,2)} ct/km\n'
-    s += f'Quote (T):  {round(100*total/totalTrips,2)} ct/km\n'
+    s += f'Quote (T):  {round(100*totalAmount/totalTrips,2)} ct/km\n'
     s += f'Nebenkosten:  {round(maintainRate,2)} €/km\n'
     for drv in self.allDriver():
-      dBill = sum(x.amount for x in self.bills if x.driver==drv) 
-      dRatio = sum(x.dist() for x in self.trips if x.driver==drv)/totalTrips
-      dTotal = dRatio*total
+      drvBill = sum(x.amount for x in self.bills if x.driver==drv) 
+      drvRatio = sum(x.dist() for x in self.trips if x.driver==drv)/totalTrips
+      drvAmount = drvRatio*totalAmount
       s += f'-------------------------------\n'
       s += f'Fahrer: {str(drv)}\n'
-      s += f'Strecke = {round(dRatio*totalTrips,2)} km\n'
-      s += f'Quote = {round(dRatio,2)}\n'
-      s += f'Anteil = {round(dTotal,2)}€\n'
-      s += f'Bezahlt = {round(dBill,2)}€\n'
-      s += f'Rest = {round(dTotal-dBill,2)}€\n'
+      s += f'Strecke = {round(drvRatio*totalTrips,2)} km\n'
+      s += f'Quote = {round(drvRatio,2)}\n'
+      s += f'Anteil = {round(drvAmount,2)}€\n'
+      s += f'Bezahlt = {round(drvBill,2)}€\n'
+      s += f'Rest = {round(drvAmount-drvBill,2)}€\n'
     return s
